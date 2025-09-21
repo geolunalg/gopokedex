932664f1baac31a8251f8fafb78f7adaf5104aab
geolunalg <geovanni_luna@yahoo.com>
2025-09-21 19:00:18 +0000

    get command from cli input

--- repl.go
+++ repl.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+		fmt.Printf("Your command was: %v\n", command)
+	}
+}
+
 func cleanInput(text string) []string {
-	// words := make([]string, 0)
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
