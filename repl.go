f2b8f4222a04618350a96174e91c00cf408d9b8d
geolunalg <geovanni_luna@yahoo.com>
2025-09-21 19:56:24 +0000

    add help and exit commands

--- repl.go
+++ repl.go
@@ -18,8 +18,18 @@ func startRepl() {
 			continue
 		}
 
-		command := words[0]
-		fmt.Printf("Your command was: %v\n", command)
+		commandName := words[0]
+		command, exist := getCommands()[commandName]
+		if exist {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
@@ -27,4 +37,25 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
