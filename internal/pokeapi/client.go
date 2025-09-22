840cd9c0704def7a3c41dbd69d12c3833384d40d
geolunalg <geovanni_luna@yahoo.com>
2025-09-22 17:58:46 +0000

    implement cache

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -1,16 +1,19 @@
 package pokeapi
 
 import (
+	"gopokedex/internal/pokecache"
 	"net/http"
 	"time"
 )
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -12,6 +12,16 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := RespShallowLocations{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+
+		return locationResp, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
@@ -19,10 +29,10 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return  RespShallowLocations{}, err
+		return RespShallowLocations{}, err
 	}
 	defer resp.Body.Close()
-	
+
 	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return RespShallowLocations{}, err
@@ -34,5 +44,6 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		return RespShallowLocations{}, err
 	}
 
+	c.cache.Add(url, dat)
 	return locationResp, nil
 }
