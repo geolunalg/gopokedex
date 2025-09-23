08a97e92aee6dcb8fd124948a29968aa1c1328af
geolunalg <geovanni_luna@yahoo.com>
2025-09-23 01:26:04 +0000

    inspect pokemon

--- repl.go
+++ repl.go
@@ -65,6 +65,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "display pokemon stats",
+			callback:    commandInspect,
+		},
 		"catch": {
 			name:        "catch <pokemon_name>",
 			description: "Attempt to catch a pokemon",
